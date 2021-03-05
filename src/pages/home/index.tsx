ec775483c102f6c012811b56d2d680a4ba96484d
Guilherme <Guilhermecontinr@gmail.com>
2021-03-05 16:57:31 -0300

    skel

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import './styles.css';
 
 
-// import { Container } from './styles';
+import Login from '../login';
 
 const home: React.FC = () => {
-  return <div><h1>home</h1></div>
+  
+
+  
+
+  return (
+  <div>
+    <Login/>
+    {/* Login With Spotify button */}
+  </div>
+  )
+  
 }
 
 export default home;
