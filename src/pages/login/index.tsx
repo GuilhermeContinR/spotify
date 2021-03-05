e64cdad7665472ecc072ae18fd30918c03f34dbc
Guilherme <Guilhermecontinr@gmail.com>
2021-03-05 16:57:21 -0300

    basic

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+
+
+const login: React.FC = () => {
+
+
+
+
+    return (
+        <div className="login">
+            <img className="login__image" src="https://olhardigital.com.br/wp-content/uploads/2019/09/20190911043801.jpg" alt="" />
+            <a> Login with spotify</a>
+        </div>
+    )
+
+}
+
+export default login;
